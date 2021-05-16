1e9a49194a5deec6d85c38fc2b9b865ac0ad171a
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 14:18:16 -0300

    ejercicio 31

---
+++ CAIGUIATIMESPAN_EJR31/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIATIMESPAN_EJR31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite un ingreso al usuario y determine si puede interpretarse como un intervalo de tiempo.
+
+            do
+            {
+                Console.Write("Ingrese un intervalo de tiempo: ");
+                string ingreso = Console.ReadLine();
+
+                if (!TimeSpan.TryParse(ingreso, out TimeSpan salida))
+                {
+                    Console.WriteLine("No puede interpretarse como un intervalo de tiempo.");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Usted ingreso: | Días: {salida.Days}|" +
+                        $" Horas: {salida.Hours}| Minutos: {salida.Minutes}| Segundos: {salida.Seconds}");
+                    break;
+                }
+
+            } while (true);
+
+            Console.ReadKey();
+
+        }
+    }
+}
